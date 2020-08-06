f0adc98195a9247a4c37cddf905c34156c9bd981
Florian Lopes <florian.lopes@outlook.com>
2020-08-06 19:25:12 +0200

    Add users-management-api: create Spring Boot app

---
+++ users-management-api/src/test/java/io/florianlopes/usersmanagement/api/UsersManagementApiApplicationTests.java
@@ -0,0 +1,11 @@
+package io.florianlopes.usersmanagement.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
